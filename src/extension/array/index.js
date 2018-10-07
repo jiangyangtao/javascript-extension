c30317b0dc5fae3386c03472df3e5e85cf3d2fd7
Yangtao.jiang <jiangyangtao1@outlook.com>
2018-10-07 19:10:35 +0800

    function change to window

--- src/extension/array/index.js
+++ src/extension/array/index.js
@@ -1,10 +1,9 @@
-import Fn from './../function'
+import win from './../window';
 
 // 移除数组中的空元素
 if (!Array.prototype.trim) {
   Array.prototype.trim = function trim() {
-    "use strict";
-    if (this === void 0 || this === null) throw new TypeError();
+    if (this === undefined || this === null) throw new TypeError();
 
     const newArray = [];
     for (let i = 0; i < this.length; i++) {
@@ -22,15 +21,12 @@ if (!Array.prototype.trim) {
 // 搜索与指定条件相匹配的元素，并返回 Array 中从零开始的索引到最后一个元素的元素范围内第一个匹配项的索引
 if (!Array.prototype.findFirstIndex) {
   Array.prototype.findFirstIndex = function findFirstIndex(object, thisArg) {
-    "use strict";
-    if (this === void 0 || this === null) throw new TypeError();
+    if (this === undefined || this === null) throw new TypeError();
 
     for (let i = 0; i < this.length; i++) {
       if (typeof object === 'function') {
         if (object.call(thisArg, this[i], i)) return i;
-      } else {
-        if (this[i] === object) return i;
-      }
+      } else if (this[i] === object) return i;
     }
     return -1;
   };
@@ -40,16 +36,13 @@ if (!Array.prototype.findFirstIndex) {
 // 搜索与指定条件相匹配的元素，并返回 Array 中从零开始的索引到最后一个元素的元素范围内最后一个匹配项的索引
 if (!Array.prototype.findLastIndex) {
   Array.prototype.findLastIndex = function findLastIndex(object, thisArg) {
-    "use strict";
-    if (this === void 0 || this === null) throw new TypeError();
+    if (this === undefined || this === null) throw new TypeError();
 
     let index = -1;
     for (let i = 0; i < this.length; i++) {
       if (typeof object === 'function') {
         if (object.call(thisArg, this[i], i)) index = i;
-      } else {
-        if (this[i] === object) index = i;
-      }
+      } else if (this[i] === object) index = i;
     }
     return index;
   };
@@ -59,16 +52,13 @@ if (!Array.prototype.findLastIndex) {
 // 搜索与指定条件相匹配的元素，并返回 Array 中包含指定元素个数、到指定索引结束的元素范围内所有匹配项的从零开始的索引
 if (!Array.prototype.findAllIndex) {
   Array.prototype.findAllIndex = function findAllIndex(object, thisArg) {
-    "use strict";
-    if (this === void 0 || this === null) throw new TypeError();
+    if (this === undefined || this === null) throw new TypeError();
 
     const indexArray = [];
     for (let i = 0; i < this.length; i++) {
       if (typeof object === 'function') {
         if (object.call(thisArg, this[i], i)) indexArray.push(i);
-      } else {
-        if (this[i] === object) indexArray.push(i);
-      }
+      } else if (this[i] === object) indexArray.push(i);
     }
     return indexArray;
   };
@@ -76,17 +66,14 @@ if (!Array.prototype.findAllIndex) {
 
 // 移除数组中的指定条件的元素
 if (!Array.prototype.remove) {
-  Array.prototype.remove = function remove(element, thisArg) {
-    "use strict";
-    if (this === void 0 || this === null) throw new TypeError();
+  Array.prototype.remove = function remove(object, thisArg) {
+    if (this === undefined || this === null) throw new TypeError();
     if (this.length <= 0) return this;
 
     for (let i = this.length - 1; i > -1; i--) {
       if (typeof object === 'function') {
         if (object.call(thisArg, this[i], i)) this.splice(i, 1);
-      } else {
-        if (this[i] === element) this.splice(i, 1);
-      }
+      } else if (this[i] === object) this.splice(i, 1);
     }
     return this;
   };
@@ -95,8 +82,7 @@ if (!Array.prototype.remove) {
 // 移除数组中的指定索引处的元素
 if (!Array.prototype.removeAt) {
   Array.prototype.removeAt = function removeAt(index) {
-    "use strict";
-    if (this === void 0 || this === null) throw new TypeError();
+    if (this === undefined || this === null) throw new TypeError();
 
     if (this.length <= 0 || index < 0 || index >= this.length) return this;
     return this.splice(index, 1);
@@ -106,8 +92,7 @@ if (!Array.prototype.removeAt) {
 // 移除数组中重复的元素
 if (!Array.prototype.removeRepeat) {
   Array.prototype.removeRepeat = function removeRepeat() {
-    "use strict";
-    if (this === void 0 || this === null) throw new TypeError();
+    if (this === undefined || this === null) throw new TypeError();
 
     return this.filter((element, index, self) => self.indexOf(element) === index);
   };
@@ -116,8 +101,7 @@ if (!Array.prototype.removeRepeat) {
 // 统计元素在数组中出现的次数
 if (!Array.prototype.sameCount) {
   Array.prototype.sameCount = function sameCount() {
-    "use strict";
-    if (this === void 0 || this === null) throw new TypeError();
+    if (this === undefined || this === null) throw new TypeError();
 
     const arr = this;
     const result = [];
@@ -138,21 +122,19 @@ if (!Array.prototype.sameCount) {
 
 // 数组关键字排序
 if (!Array.prototype.keySort) {
-  Array.prototype.keySort = function keySort(key, orderby = 'asc') {
-    "use strict";
-    if (this === void 0 || this === null) throw new TypeError();
+  Array.prototype.keySort = function keySort(key, order) {
+    if (this === undefined || this === null) throw new TypeError();
 
-    const _orderby = true;
-    if (orderby === 'desc') _orderby = false;
-    return this.sort(Fn.keySort(key, _orderby));
-  }
+    let _order = true;
+    if (order === 'desc') _order = false;
+    return this.sort(win.keySort(key, _order));
+  };
 }
 
 // 检测数组中是否包含指定元素
 if (!Array.prototype.contains) {
   Array.prototype.contains = function contains(element) {
-    "use strict";
-    if (this === void 0 || this === null) throw new TypeError();
+    if (this === undefined || this === null) throw new TypeError();
 
     if (this.length <= 0) return false;
     for (let i = 0; i < this.length; i++) {
@@ -165,8 +147,7 @@ if (!Array.prototype.contains) {
 // 数字数组由小到大排序
 if (!Array.prototype.minSortMax) {
   Array.prototype.minSortMax = function minSortMax() {
-    "use strict";
-    if (this === void 0 || this === null) throw new TypeError();
+    if (this === undefined || this === null) throw new TypeError();
 
     let oValue;
     for (let i = 0; i < this.length; i++) {
@@ -185,8 +166,7 @@ if (!Array.prototype.minSortMax) {
 // 数字数组由大到小排序
 if (!Array.prototype.maxSortMin) {
   Array.prototype.maxSortMin = function maxSortMin() {
-    "use strict";
-    if (this === void 0 || this === null) throw new TypeError();
+    if (this === undefined || this === null) throw new TypeError();
 
     let oValue;
     for (let i = 0; i < this.length; i++) {
@@ -205,8 +185,7 @@ if (!Array.prototype.maxSortMin) {
 // 获取数字数组中最大值
 if (!Array.prototype.getMaxValue) {
   Array.prototype.getMaxValue = function getMaxValue() {
-    "use strict";
-    if (this === void 0 || this === null) throw new TypeError();
+    if (this === undefined || this === null) throw new TypeError();
 
     let oValue = 0;
     for (let i = 0; i < this.length; i++) {
@@ -221,8 +200,7 @@ if (!Array.prototype.getMaxValue) {
 // 获取数字数组中最小值
 if (Array.prototype.getMinValue) {
   Array.prototype.getMinValue = function getMinValue() {
-    "use strict";
-    if (this === void 0 || this === null) throw new TypeError();
+    if (this === undefined || this === null) throw new TypeError();
 
     let oValue = 0;
     for (let i = 0; i < this.length; i++) {
@@ -237,8 +215,7 @@ if (Array.prototype.getMinValue) {
 // 在数组头部追加元素
 if (!Array.prototype.insert) {
   Array.prototype.insert = function insert(value) {
-    "use strict";
-    if (this === void 0 || this === null) throw new TypeError();
+    if (this === undefined || this === null) throw new TypeError();
 
     const newArray = [];
     newArray[0] = value;
@@ -252,8 +229,7 @@ if (!Array.prototype.insert) {
 // 将某值设置给 Array 中指定位置的元素
 if (!Array.prototype.setValue) {
   Array.prototype.setValue = function setValue(index, value) {
-    "use strict";
-    if (this === void 0 || this === null) throw new TypeError();
+    if (this === undefined || this === null) throw new TypeError();
 
     if (this.length <= 0 || index < 0 || index >= this.length) return this;
     return this.splice(index, 1, value);
@@ -263,8 +239,7 @@ if (!Array.prototype.setValue) {
 // 替换数组中指定条件的所有元素
 if (!Array.prototype.replace) {
   Array.prototype.replace = function replace(conditions, value) {
-    "use strict";
-    if (this === void 0 || this === null) throw new TypeError();
+    if (this === undefined || this === null) throw new TypeError();
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] === conditions) this.splice(i, 1, value);
